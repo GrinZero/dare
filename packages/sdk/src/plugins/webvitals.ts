70d6b9a2c8f2b7b8cac58d26a362697cc1be71b0
GrinZero <774933704@qq.com>
2024-01-11 00:59:38 +0800

    feat: add some observer

--- packages/sdk/src/plugins/webvitals.ts
+++ packages/sdk/src/plugins/webvitals.ts
@@ -12,11 +12,44 @@ export const webVitalsPlugin: DarePlugin<WebVitalsPluginOptions> = (
 
   return {
     main: (context) => {
-      const obsercer = new PerformanceObserver((list) => {
-        console.log(list.getEntries());
+      const FCPBbsercer = new PerformanceObserver((list) => {
+        for (const entry of list.getEntriesByName("first-contentful-paint")) {
+          console.log("FCP candidate:", entry.startTime, entry);
+        }
         context.core.report(list.getEntries());
       });
-      obsercer.observe({ entryTypes: ["navigation", "resource", "paint"] });
+      FCPBbsercer.observe({
+        entryTypes: ["navigation", "resource", "paint"],
+        buffered: true,
+      });
+
+      const LCPObserver = new PerformanceObserver((entryList) => {
+        for (const entry of entryList.getEntries()) {
+          console.log("LCP candidate:", entry.startTime, entry);
+        }
+      });
+      LCPObserver.observe({ type: "largest-contentful-paint", buffered: true });
+
+      const CLSObserver = new PerformanceObserver((entryList) => {
+        for (const entry of entryList.getEntries()) {
+          console.log('CLS:', entry);
+        }
+      })
+      CLSObserver.observe({type: 'layout-shift', buffered: true});
+
+      const FIDObserver = new PerformanceObserver((entryList) => {
+        for (const entry of entryList.getEntries()) {
+          console.log('FID:', entry);
+        }
+      })
+      FIDObserver.observe({type: 'first-input', buffered: true});
+
+      return () => {
+        FCPBbsercer.disconnect();
+        LCPObserver.disconnect();
+        CLSObserver.disconnect();
+        FIDObserver.disconnect();
+      };
     },
     priority: "high",
   };
