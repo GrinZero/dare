5c5927acd3422285701ed3cfe4f5d69ba9f5529e
GrinZero <774933704@qq.com>
2024-01-18 00:36:57 +0800

    fix: add debugger code

--- packages/sdk/src/plugins/error.ts
+++ packages/sdk/src/plugins/error.ts
@@ -5,11 +5,6 @@ export type ErrorPluginOptions = {
   onError?: (error: ErrorEvent) => void;
 };
 
-const isReportingError = (error: ErrorEvent) => {
-  // 假设我们在上报错误时添加了一个特殊的属性
-  return error.error && error.error.__isReportingError;
-};
-
 export const errorPlugin: DarePlugin<ErrorPluginOptions> = (_options) => {
   const options: ErrorPluginOptions = {};
   Object.assign(options, _options);
@@ -31,14 +26,13 @@ export const errorPlugin: DarePlugin<ErrorPluginOptions> = (_options) => {
       const url = context.core.reporter.options?.url;
 
       const handleError = (data: ErrorEvent) => {
-        if (isReportingError(data) || errorSet.has(data.error)) {
+        if (errorSet.has(data.error)) {
           // 如果是上报错误或者已经处理过的错误，则不再处理
           return;
         }
         add(data.error);
         options.onError && options.onError(data);
 
-        data.error.__isReportingError = true;
         context.core.report({
           type: "error",
           data: {
@@ -47,7 +41,6 @@ export const errorPlugin: DarePlugin<ErrorPluginOptions> = (_options) => {
             env: context.core.getEnv(),
           },
         });
-        delete data.error.__isReportingError;
       };
       const handleReject = (event: PromiseRejectionEvent) => {
         const { reason } = event;
